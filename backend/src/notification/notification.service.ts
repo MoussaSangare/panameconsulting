0e2d37c6af779da6df075ab674e7d4657d07e419
Moussa Sangare <moussa.sangare.ma@gmail.com>
2025-12-21 23:19:56 +0000

    correcting

--- backend/src/notification/notification.service.ts
+++ backend/src/notification/notification.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
+import nodemailer from 'nodemailer';
 import { Rendezvous } from '../schemas/rendezvous.schema';
 import { Procedure, ProcedureStatus, StepStatus } from '../schemas/procedure.schema';
 import { ConfigService } from '@nestjs/config';
